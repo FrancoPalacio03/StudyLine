895ded3b6f3a1bcbfd8248ced4fcf641af65aec1
FrancoPalacio <francopalacio03@gmail.com>
2024-10-29 14:20:31 -0300

    Initial commit

---
+++ app/src/main/java/com/example/studyline/ui/gallery/GalleryViewModel.kt
@@ -0,0 +1,13 @@
+package com.example.studyline.ui.gallery
+
+import androidx.lifecycle.LiveData
+import androidx.lifecycle.MutableLiveData
+import androidx.lifecycle.ViewModel
+
+class GalleryViewModel : ViewModel() {
+
+    private val _text = MutableLiveData<String>().apply {
+        value = "This is gallery Fragment"
+    }
+    val text: LiveData<String> = _text
+}
